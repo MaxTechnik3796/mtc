3e721d6b1759e4b2e478e562acdc61ee24d84891
surik <suriken222a@gmail.com>
2026-05-05 18:08:48 +0200

    1.21.1 hotovo

--- src/main/java/cz/maxtechnik/mtc/MtcMod.java
+++ src/main/java/cz/maxtechnik/mtc/MtcMod.java
@@ -1,22 +1,22 @@
 package cz.maxtechnik.mtc;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 import java.util.Objects;
@@ -25,26 +25,25 @@ import java.util.Objects;
 public class MtcMod{
 	public static final String MODID="mtc";
 	public static final Logger LOGGER=LogUtils.getLogger();
-	public static final DeferredRegister<Item> ITEMS=DeferredRegister.create(ForgeRegistries.ITEMS,MtcMod.MODID);
+	public static final DeferredRegister.Items ITEMS=DeferredRegister.createItems(MODID);
 	public void registerItems(){
 		MtcModBlocks.REGISTRY.getEntries().forEach(block->{
-			assert block.getId()!=null;
-			ITEMS.register(block.getId().getPath(),()->new BlockItem(block.get(),new Item.Properties()));
+            ITEMS.register(block.getId().getPath(),()->new BlockItem(block.get(),new Item.Properties()));
 		});
 	}
 	public static final DeferredRegister<CreativeModeTab>TABS=DeferredRegister.create(Registries.CREATIVE_MODE_TAB,MODID);
-	public static final RegistryObject<CreativeModeTab>TAB=TABS.register("nadve_tab",()->CreativeModeTab.builder()
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB=TABS.register("nadve_tab",()->CreativeModeTab.builder()
 			.title(Component.translatable("creative_tab.mtc.blocks"))
-			.icon(()->new ItemStack(Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(ResourceLocation.fromNamespaceAndPath("mtc","c2_cobblestone")))))
+			.icon(()->new ItemStack(Objects.requireNonNull(BuiltInRegistries.ITEM.get(ResourceLocation.parse("mtc:c2_cobblestone")))))
 			.displayItems((parameters, output) ->ITEMS.getEntries().forEach(item -> output.accept(item.get()))).build());
-	public MtcMod(){
-		IEventBus bus=FMLJavaModLoadingContext.get().getModEventBus();
+	public MtcMod(IEventBus bus){
+		bus.addListener(this::commonSetup);
 		MtcModBlocks.REGISTRY.register(bus);
 		registerItems();
 		ITEMS.register(bus);
 		TABS.register(bus);
 		bus.addListener(this::commonSetup);
-		MinecraftForge.EVENT_BUS.register(this);
+		NeoForge.EVENT_BUS.register(this);
 	}
 	private void commonSetup(final FMLCommonSetupEvent event){
 		LOGGER.info("MT-Compressed: Common Setup");
@@ -53,7 +52,7 @@ public class MtcMod{
 	public void onServerStarting(ServerStartingEvent event){
 		LOGGER.info("MT-Compressed: Server Starting");
 	}
-	@Mod.EventBusSubscriber(modid=MODID,bus=Mod.EventBusSubscriber.Bus.MOD,value=Dist.CLIENT)
+	@EventBusSubscriber(modid=MODID,bus=EventBusSubscriber.Bus.MOD,value=Dist.CLIENT)
 	public static class ClientModEvents{
 		@SubscribeEvent
 		public static void onClientSetup(FMLClientSetupEvent event){
--- src/main/java/cz/maxtechnik/mtc/MtcModBlocks.java
+++ src/main/java/cz/maxtechnik/mtc/MtcModBlocks.java
@@ -1,70 +1,68 @@
 package cz.maxtechnik.mtc;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 import static net.minecraft.world.level.block.Blocks.*;
 public class MtcModBlocks{
-	public static final DeferredRegister<Block> REGISTRY=DeferredRegister.create(ForgeRegistries.BLOCKS,MtcMod.MODID);
-	static BlockBehaviour.Properties c_cobblestone_props=BlockBehaviour.Properties.copy(COBBLESTONE);
-	public static final RegistryObject<Block>C1_COBBLESTONE=REGISTRY.register("c1_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C2_COBBLESTONE=REGISTRY.register("c2_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C3_COBBLESTONE=REGISTRY.register("c3_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C4_COBBLESTONE=REGISTRY.register("c4_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C5_COBBLESTONE=REGISTRY.register("c5_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C6_COBBLESTONE=REGISTRY.register("c6_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C7_COBBLESTONE=REGISTRY.register("c7_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C8_COBBLESTONE=REGISTRY.register("c8_cobblestone",()->new Block(c_cobblestone_props));
-	public static final RegistryObject<Block>C9_COBBLESTONE=REGISTRY.register("c9_cobblestone",()->new Block(c_cobblestone_props));
+	public static final DeferredRegister.Blocks REGISTRY=DeferredRegister.createBlocks(MtcMod.MODID);
+	static BlockBehaviour.Properties c_cobblestone_props=BlockBehaviour.Properties.ofFullCopy(COBBLESTONE);
+	public static final DeferredBlock<Block> C1_COBBLESTONE=REGISTRY.registerSimpleBlock("c1_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C2_COBBLESTONE=REGISTRY.registerSimpleBlock("c2_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C3_COBBLESTONE=REGISTRY.registerSimpleBlock("c3_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C4_COBBLESTONE=REGISTRY.registerSimpleBlock("c4_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C5_COBBLESTONE=REGISTRY.registerSimpleBlock("c5_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C6_COBBLESTONE=REGISTRY.registerSimpleBlock("c6_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C7_COBBLESTONE=REGISTRY.registerSimpleBlock("c7_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C8_COBBLESTONE=REGISTRY.registerSimpleBlock("c8_cobblestone",c_cobblestone_props);
+	public static final DeferredBlock<Block>C9_COBBLESTONE=REGISTRY.registerSimpleBlock("c9_cobblestone",c_cobblestone_props);
 
-	static BlockBehaviour.Properties c_dirt_props=BlockBehaviour.Properties.copy(DIRT);
-	public static final RegistryObject<Block>C1_DIRT=REGISTRY.register("c1_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C2_DIRT=REGISTRY.register("c2_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C3_DIRT=REGISTRY.register("c3_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C4_DIRT=REGISTRY.register("c4_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C5_DIRT=REGISTRY.register("c5_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C6_DIRT=REGISTRY.register("c6_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C7_DIRT=REGISTRY.register("c7_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C8_DIRT=REGISTRY.register("c8_dirt",()->new Block(c_dirt_props));
-	public static final RegistryObject<Block>C9_DIRT=REGISTRY.register("c9_dirt",()->new Block(c_dirt_props));
-
-
-	static BlockBehaviour.Properties c_gravel_props=BlockBehaviour.Properties.copy(GRAVEL);
-	public static final RegistryObject<Block>C1_GRAVEL=REGISTRY.register("c1_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C2_GRAVEL=REGISTRY.register("c2_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C3_GRAVEL=REGISTRY.register("c3_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C4_GRAVEL=REGISTRY.register("c4_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C5_GRAVEL=REGISTRY.register("c5_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C6_GRAVEL=REGISTRY.register("c6_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C7_GRAVEL=REGISTRY.register("c7_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C8_GRAVEL=REGISTRY.register("c8_gravel",()->new Block(c_gravel_props));
-	public static final RegistryObject<Block>C9_GRAVEL=REGISTRY.register("c9_gravel",()->new Block(c_gravel_props));
+	static BlockBehaviour.Properties c_dirt_props=BlockBehaviour.Properties.ofFullCopy(DIRT);
+	public static final DeferredBlock<Block>C1_DIRT=REGISTRY.registerSimpleBlock("c1_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C2_DIRT=REGISTRY.registerSimpleBlock("c2_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C3_DIRT=REGISTRY.registerSimpleBlock("c3_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C4_DIRT=REGISTRY.registerSimpleBlock("c4_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C5_DIRT=REGISTRY.registerSimpleBlock("c5_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C6_DIRT=REGISTRY.registerSimpleBlock("c6_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C7_DIRT=REGISTRY.registerSimpleBlock("c7_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C8_DIRT=REGISTRY.registerSimpleBlock("c8_dirt",c_dirt_props);
+	public static final DeferredBlock<Block>C9_DIRT=REGISTRY.registerSimpleBlock("c9_dirt",c_dirt_props);
 
 
-	static BlockBehaviour.Properties c_deepslate_props=BlockBehaviour.Properties.copy(COBBLED_DEEPSLATE);
-	public static final RegistryObject<Block>C1_DEEPSLATE=REGISTRY.register("c1_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C2_DEEPSLATE=REGISTRY.register("c2_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C3_DEEPSLATE=REGISTRY.register("c3_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C4_DEEPSLATE=REGISTRY.register("c4_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C5_DEEPSLATE=REGISTRY.register("c5_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C6_DEEPSLATE=REGISTRY.register("c6_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C7_DEEPSLATE=REGISTRY.register("c7_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C8_DEEPSLATE=REGISTRY.register("c8_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
-	public static final RegistryObject<Block>C9_DEEPSLATE=REGISTRY.register("c9_deepslate",()->new RotatedPillarBlock(c_deepslate_props));
+	static BlockBehaviour.Properties c_gravel_props=BlockBehaviour.Properties.ofFullCopy(GRAVEL);
+	public static final DeferredBlock<Block>C1_GRAVEL=REGISTRY.registerSimpleBlock("c1_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C2_GRAVEL=REGISTRY.registerSimpleBlock("c2_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C3_GRAVEL=REGISTRY.registerSimpleBlock("c3_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C4_GRAVEL=REGISTRY.registerSimpleBlock("c4_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C5_GRAVEL=REGISTRY.registerSimpleBlock("c5_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C6_GRAVEL=REGISTRY.registerSimpleBlock("c6_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C7_GRAVEL=REGISTRY.registerSimpleBlock("c7_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C8_GRAVEL=REGISTRY.registerSimpleBlock("c8_gravel",c_gravel_props);
+	public static final DeferredBlock<Block>C9_GRAVEL=REGISTRY.registerSimpleBlock("c9_gravel",c_gravel_props);
 
 
-	static BlockBehaviour.Properties c_cobbled_deepslate_props=BlockBehaviour.Properties.copy(DEEPSLATE);
-	public static final RegistryObject<Block>C1_COBBLED_DEEPSLATE=REGISTRY.register("c1_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C2_COBBLED_DEEPSLATE=REGISTRY.register("c2_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C3_COBBLED_DEEPSLATE=REGISTRY.register("c3_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C4_COBBLED_DEEPSLATE=REGISTRY.register("c4_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C5_COBBLED_DEEPSLATE=REGISTRY.register("c5_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C6_COBBLED_DEEPSLATE=REGISTRY.register("c6_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C7_COBBLED_DEEPSLATE=REGISTRY.register("c7_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C8_COBBLED_DEEPSLATE=REGISTRY.register("c8_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
-	public static final RegistryObject<Block>C9_COBBLED_DEEPSLATE=REGISTRY.register("c9_cobbled_deepslate",()->new Block(c_cobbled_deepslate_props));
+	static BlockBehaviour.Properties c_deepslate_props=BlockBehaviour.Properties.ofFullCopy(COBBLED_DEEPSLATE);
+	public static final DeferredBlock<Block>C1_DEEPSLATE=REGISTRY.registerSimpleBlock("c1_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C2_DEEPSLATE=REGISTRY.registerSimpleBlock("c2_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C3_DEEPSLATE=REGISTRY.registerSimpleBlock("c3_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C4_DEEPSLATE=REGISTRY.registerSimpleBlock("c4_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C5_DEEPSLATE=REGISTRY.registerSimpleBlock("c5_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C6_DEEPSLATE=REGISTRY.registerSimpleBlock("c6_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C7_DEEPSLATE=REGISTRY.registerSimpleBlock("c7_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C8_DEEPSLATE=REGISTRY.registerSimpleBlock("c8_deepslate",c_deepslate_props);
+	public static final DeferredBlock<Block>C9_DEEPSLATE=REGISTRY.registerSimpleBlock("c9_deepslate",c_deepslate_props);
+
+
+	static BlockBehaviour.Properties c_cobbled_deepslate_props=BlockBehaviour.Properties.ofFullCopy(DEEPSLATE);
+	public static final DeferredBlock<Block>C1_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c1_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C2_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c2_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C3_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c3_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C4_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c4_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C5_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c5_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C6_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c6_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C7_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c7_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C8_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c8_cobbled_deepslate",c_cobbled_deepslate_props);
+	public static final DeferredBlock<Block>C9_COBBLED_DEEPSLATE=REGISTRY.registerSimpleBlock("c9_cobbled_deepslate",c_cobbled_deepslate_props);
 }
